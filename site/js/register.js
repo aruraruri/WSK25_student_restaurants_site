445181ceca809f7992718b9aef92c06b9ea119e3
aruraruri <aurilait@gmail.com>
2025-04-08 13:32:33 +0300

    Add registration functionality, add email input and improve JavaScript handling; include 'defer' attribute for script loading

--- site/js/register.js
+++ site/js/register.js
@@ -1,1 +1,37 @@
 "use strict";
+
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const dator = {
+    username: form.username.value,
+    password: form.password.value,
+    email: form.email.value,
+  };
+
+  //   const formData = new FormData(form);
+  //   const data = Object.fromEntries(formData.entries());
+
+  const response = await fetch(
+    "https://media2.edu.metropolia.fi/restaurant/api/v1/users",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dator),
+    }
+  );
+
+  if (response.ok) {
+    console.log("Registration successful!");
+    // Optionally redirect or show a success message
+  } else {
+    console.error("Registration failed.");
+    // Optionally show an error message
+  }
+});
+
+console.log(form);
